14a2b56800d4956f1cc5ad75f9d1e4eafc6a67b0
ahmed <toto@gmail.com>
2022-03-23 11:11:23 +0100

    creation de projet spring

---
+++ MediathequeLibrary/src/main/java/org/project/MediathequeLibraryApplication.java
@@ -0,0 +1,13 @@
+package org.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediathequeLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediathequeLibraryApplication.class, args);
+	}
+
+}
